0eed100f6825f8677b635ddd60f19793a3e17bbd
ToneIntell <esystony@duck.com>
2026-08-31 19:44:57 +1000

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "genset-log-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
